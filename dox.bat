e6143fd3cff4c6d300664d6b67958eeee5bf67bf
LiHuarong <168278282@qq.com>
2025-05-12 09:38:25 +0800

    忽略此更改，不同设备不同路径导致快捷方式失效

--- dox.bat
+++ dox.bat
@@ -1,5 +1,5 @@
 @echo off
-set PYTHON_PATH="c:\Users\is\AppData\Local\Microsoft\WindowsApps\python3.13.exe"
+set PYTHON_PATH="E:\APP\Python313\python3.13.exe"
 set PYTHON_SCRIPT="main.py"
 
 %PYTHON_PATH% %PYTHON_SCRIPT%
