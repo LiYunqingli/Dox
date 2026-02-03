b41bff0beb6711123b3104c979e33e4a4b60b1b4
LiHuarong <168278282@qq.com>
2026-02-03 21:58:46 +0800

    暂存

--- lib/src/update.py
+++ lib/src/update.py
@@ -5,6 +5,7 @@ def update():
     
 
     local_version = get_config()["About"]["Version"]
+    dox_run_style = get_config()["Config"]["RunStyle"]
     _print("当前dox版本为: " + local_version + "\n")
 
-    new_version = "x.x.x"
+    new_version_url = "http://8.138.142.121/LiHuarong/Dox/raw/branch/main/latest_version"
